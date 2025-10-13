abe562ee7bfe73e9bf77844af2d9d99acfb3c516
MarengoAlberto <alberto@bitstrapped.com>
2025-10-13 16:34:18 -0400

    reverted

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -16,49 +16,50 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
-  Kp = Kpi;
-  Ki = Kii;
-  Kd = Kdi;
-  output_lim_max = output_lim_maxi;
-  output_lim_min = output_lim_mini;
-  sum_cte = 0.0;
-  diff_cte = 0.0;
-  cte = 0.0;
-  is_first = true;
+    /**
+    * TODO: Initialize PID coefficients (and errors, if needed)
+    **/
+    prop_cte = 0.0; // proportional errro
+    diff_cte = 0.0; // differential error
+    int_cte = 0.0; // integral error
+
+    Kp = Kpi; // proportional coefficient
+    Kd = Kdi; // differential coefficient
+    Ki = Kii; // integral coefficient
+
+    output_lim_min = output_lim_mini; // Minimal Output limit
+    output_lim_max = output_lim_maxi; // Maximum Output limit
+
+    dt = 0.0; // Delta time
 }
 
 
-void PID::UpdateError(double cte_current) {
-  if (is_first) {
-    	diff_cte = 0;
-    	is_first = false;
-  } else {
-      if (std::abs(delta_time) < 0.0001)
-        diff_cte = 0;
-      else
-		diff_cte = (cte_current - cte) / delta_time * 0.7 + 0.3 * diff_cte;
-  }
-  sum_cte = sum_cte + cte_current * delta_time;
-  cte = cte_current;
+void PID::UpdateError(double cte) {
+    /**
+    * TODO: Update PID errors based on cte.
+    **/
+    diff_cte = (dt > 0.0) ? (cte - prop_cte) / dt : 0.0;
+    int_cte += cte * dt;
+    prop_cte = cte;
 }
 
-double PID::TotalError(bool debug) {
-  double control;
+double PID::TotalError() {
+    /**
+    * TODO: Calculate and return the total error
+    * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
+    */
+   double control = (Kp * prop_cte) + (Kd * diff_cte) + (Ki * int_cte);
 
-  // Calculate control output
-  control = -Kp * cte - Kd * diff_cte - Ki * sum_cte; // NOTE: error terms already include the time
+   control = max(min(control, output_lim_max), output_lim_min);
 
-  if (debug) {
-  	std::cout << "Cte " << cte << " Control: " << control << " P: " << -Kp * cte << " I: " << - Ki * sum_cte << " D: " << - Kd * diff_cte << " T: " << delta_time << std::endl;
-  }
-
-  // Ensure output is in the desired range
-  control = max(control, output_lim_min);
-  control = min(control, output_lim_max);
-
-  return control;
+   return control;
 }
 
-void PID::UpdateDeltaTime(double new_delta_time) {
-   delta_time = new_delta_time;
+double PID::UpdateDeltaTime(double new_delta_time) {
+    /**
+    * TODO: Update the delta time with new value
+    */
+    dt = new_delta_time;
+
+    return dt;
 }
--- project/pid_controller/pid_controller.h
+++ project/pid_controller/pid_controller.h
@@ -10,70 +10,44 @@
 class PID {
 public:
 
-   /**
-   * TODO: Create the PID class
-   **/
+    /**
+    * TODO: Create the PID class
+    **/
 
-    /*
-    * Errors
-    */
-  	double cte;
-  	double diff_cte;
-  	double sum_cte;
+    // Errors
+    double prop_cte;
+    double diff_cte;
+    double int_cte;
 
-    /*
-    * Coefficients
-    */
-  	double Kp;
-  	double Ki;
-  	double Kd;
+    // Coefficients
+    double Kp;
+    double Kd;
+    double Ki;
 
-    /*
-    * Output limits
-    */
-  	double output_lim_max;
-  	double output_lim_min;
+    // Output limits
+    double output_lim_min;
+    double output_lim_max;
 
-    /*
-    * Delta time
-    */
-  	double delta_time;
+    // Delta time
+    double dt;
 
-    /*
-    * Constructor
-    */
+    // Constructor
     PID();
 
-    /*
-    * Destructor.
-    */
+    // Destructor.
     virtual ~PID();
 
-    /*
-    * Initialize PID.
-    */
+    // Initialize PID.
     void Init(double Kp, double Ki, double Kd, double output_lim_max, double output_lim_min);
 
-    /*
-    * Update the PID error variables given cross track error.
-    */
+    // Update the PID error variables given cross track error.
     void UpdateError(double cte);
 
-    /*
-    * Calculate the total PID error.
-    */
-    double TotalError(bool debug=false);
+    // Calculate the total PID error.
+    double TotalError();
 
-    /*
-    * Update the delta time.
-    */
-    void UpdateDeltaTime(double new_delta_time);
-
-protected:
-	/*
-    * Store if this is the first iteration, as diff_cte is set to 0 in that case.
-    */
-	bool is_first;
+    // Update the delta time.
+    double UpdateDeltaTime(double new_delta_time);
 };
 
 #endif //PID_CONTROLLER_H
